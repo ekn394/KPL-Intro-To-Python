9cec53e60f1b0f2e08283c922c5f5c92e119963a
Evan Nordquist <nordquist.evan@gmail.com>
2018-06-05 17:58:45 -0400

    Added 'broken code' problems for day 1 of class.

---
+++ day1BrokenCode.py
@@ -0,0 +1,23 @@
+# Broken Code - Day 1
+# Try to fix the code below so that the output matches the comments
+
+print("Hi my name is Evan')
+#Should print Hi my name is Evan
+
+print('Hi my name is still Evan'
+#Should print Hi my name is still Evan
+
+a = 4
+
+print(A)
+#Should print 4
+
+
+print (7/4)
+# should print 1.75
+
+
+print ("hello" + "world")
+# I want the output to be hello world.  Not helloworld.
+
+
